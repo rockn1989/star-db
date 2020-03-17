ee56fd2a4f7edd0fc9707491ea643deb4f2a45ab
rockn1989 <gold_100@bk.ru>
2020-03-17 07:59:54 +0300

    рефакторинг

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -3,18 +3,31 @@ import React, { Component } from "react";
 import Header from "../header";
 import RandomPlanet from "../random-planet";
 import PeoplePage from "../people-page";
+import ItemList from "../item-list";
+import PersonDetails from "../person-details";
+import SwapiService from "../../services/swapi-service";
 
 import "./app.css";
 
 export default class App extends Component {
+	swapiService = new SwapiService();
 	render() {
 		return (
 			<div>
 				<Header />
 				<RandomPlanet />
-				<PeoplePage />
-				<PeoplePage />
-				<PeoplePage />
+				<PeoplePage getData={this.swapiService.getAllPlanets} />
+				<div className="row mb2">
+					<div className="col-md-6">
+						<ItemList
+							onItemSelected={this.onPersonSelected}
+							getData={this.swapiService.getAllPlanets}
+						/>
+					</div>
+					<div className="col-md-6">
+						<PersonDetails personId={this.state.selectedPerson} />
+					</div>
+				</div>
 			</div>
 		);
 	}
