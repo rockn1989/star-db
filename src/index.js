ad4b980f33f2d020216012b8d71e9888521cd14d
rockn1989 <gold_100@bk.ru>
2020-04-29 00:00:05 +0300

    Настройка роутинга приложения

--- src/index.js
+++ src/index.js
@@ -10,3 +10,10 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // this.props.children
 // React.Children
 // HOC - фун-я высшего порядка
+// React-router: подключаем Browsersec, Route from react-router-dom
+// Route задается path и имя компонента для отрисовки component={MyComponent}
+// для точности path, прописывается аттрибут exact
+// в Route можно передавать render фун-ю
+// ссылки для страниц задаются через компонент Link to="/url"
+// withRouter - фун-я HOC , для Route (чтобы иметь доступ к match,history,location)
+// Switch - HOC для Route и Redirect, если в конце будет пустой Route , то выполнится он
